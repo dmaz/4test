c561a16ee7f312b79574a96268011579e0f89d2c
David Maziarka <dmazmail@yahoo.com>
2021-10-14 20:53:19 -0500

    added graph

--- Main.gd
+++ Main.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
+var tick:int = 0
+var data:Array
+var time:int
+var zoom:int = 3
+var size:int
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _ready() -> void:
+	size = get_viewport().size.x
+	data.resize(size)
+	for i in size:
+		data[i] = Vector2(i,400-16*zoom)
+
+	time = Time.get_ticks_msec()
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	var frameTime = Time.get_ticks_msec() - time
 
+	time = Time.get_ticks_msec()
+	tick += 1
+	$Label.text = "%s"%tick
+	data.append(Vector2(tick % size, 400-frameTime*zoom))
+	data.pop_front()
+	data[0] = Vector2(data[0].x, 50)
+	data[0] = Vector2(data[1].x, 400)
+	update()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _draw() -> void:
+	var points = PackedVector2Array(data)
+	draw_multiline(points,Color.ALICE_BLUE)
